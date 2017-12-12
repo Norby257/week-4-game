c03604d96f30efe6a76b2670ff7c4c3b810df6a2
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-11 18:30:11 -0600

    Jquery linked to external file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,76 @@
 console.log("we are linked");
 
-//what i need to do 
-//declare variables that i need 
+$(document).ready(function() {
+    
+        //vars global to runtime 
+    
+        var wins;
+        var losses; 
+        var totalScore;
+        var computerNumber;
+        
+      
+    //write a for loop that assgins a random number to the crystals when the page is loaded ? how to do this?
 
+        for (var i = 0; i <crystalOptions.length; i++) {
+            var crystalNumberOptions = Math.floor(Math.random() * (12-1)) + 1;            
+            var crystalValue = ($(this).attr("data-crystalValue"));
+            
 
+        }
+        var computerNumber = Math.floor( Math.random() * (120-19)) +1; 
+        crystalOptions = [1,2,3,4]; //consider changing this to a method 
+    
+        $("#random-number").text(computerNumber);
+        console.log(computerNumber);
+    
+        var counter = 0;
+    
+        for (var i = 0; i < crystalOptions.length; i++) {
+            //assign image 
+            var crystalPic = $("<img>");
+            //add a class using jQuery 
+            crystalPic.addClass("crystal-Pic");
 
-// global variables 
-var wins;
-var losses; 
-var totalScore;
-var computerNumber;
+            //add an attribute for img src
+            crystalPic.attr("src", "http://www.crystalclearintentions.co.nz/wp-content/uploads/crystals.jpg");
+            //add a data attribute called crystal value
 
-//arrays for randomly generating the numbers
-
-var crystalNumber = Math.floor(Math.random() * (12-1)) + 1;
-var computerNumber = Math.floor( Math.random() * (120-19)) +1; 
-
-
-//click event generate a random number when gem is clicked (should this be a function-the rando number part)
-$("#second-gem").on("click", function() {
-    var crystalNumber = Math.floor(Math.random() * (12-1)) + 1;
-    console.log(crystalNumber);
+            crystalPic.attr("data-crystalValue", crystalNumberOptions[i]);
+            $("#crystals").append(crystalPic);
+            console.log(crystalNumberOptions);
+        }
+    
+    //click event 
+    $(".crystal-pic").on("click", function() {
+        var crystalValue = ($(this).attr("data-crystalValue"));
+    
+        crystalValue = parseInt(crystalValue);
+        counter += crystalValue;
+        console.log(crystalValue);
+        alert("Points" + crystalValue);
+        // $("#total-score").text(counter);
+    })
+    
+    // when I try to display total score it over rides the computer one. not sure why 
+    // also only one crystal has a num but it;s not working as expected 
+    //maybe make it an object that has a method where a random number is generated 
+    
+    // if (totalScore === computerNumber) {
+    //     //console log and alert just for testing purposes 
+    //     console.log("Yay, you win! Rock on. Click the refresh button to play again!");
+    //     alert("Yay, you win! Rock on. Click the refresh button to play again!");
+    //     wins++;
+    // } else if 
+    //     (totalScore > computerNumber) {
+    //         console.log("Oh you, you lost. Click the refresh button to try again");
+    //         alert("Oh you, you lost. Click the refresh button to try again");
+    //         losses++;
+            
+    //     }
     
     
-});
+    
+    
+     });
 
