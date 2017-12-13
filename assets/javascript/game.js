90a9b97e8f2a33c468bda60698b3a77edefd8a86
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-12 22:57:35 -0600

    added data attributes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,58 @@
 console.log("we are linked");
+    //making sure DOM is ready before we start manipulating it
 
 $(document).ready(function() {
-    
-        //vars global to runtime 
+        
+        //global variables, things I want to update on page refresh or when page resets 
     
         var wins;
         var losses; 
-        var totalScore;
-        var computerNumber;
+        var counter = 0;
+        var computerNumber = Math.floor( Math.random() * (120-19)); 
+        
+        //display computerNumber on the screen
+        $("#random-number").text(computerNumber);
+        //console.log for testing purposes 
+        console.log(computerNumber);
+        
+        //choices for random numbers for crystals --but this has to be for each one 
+        var crystalValue = Math.floor(Math.random() * (12-1));            
         
       
-    //write a for loop that assgins a random number to the crystals when the page is loaded ? how to do this?
 
-        for (var i = 0; i <crystalOptions.length; i++) {
-            var crystalNumberOptions = Math.floor(Math.random() * (12-1)) + 1;            
-            var crystalValue = ($(this).attr("data-crystalValue"));
-            
+//for each crystal, assign an attribute //
 
-        }
-        var computerNumber = Math.floor( Math.random() * (120-19)) +1; 
-        crystalOptions = [1,2,3,4]; //consider changing this to a method 
+$("#crystal-1").attr("data-crystalValue");
+
+// this attribute will contain the value of the random number 
+
+//each time I click a crystal, add crystal value to counter
+
+        
     
         $("#random-number").text(computerNumber);
         console.log(computerNumber);
     
-        var counter = 0;
-    
-        for (var i = 0; i < crystalOptions.length; i++) {
-            //assign image 
-            var crystalPic = $("<img>");
-            //add a class using jQuery 
-            crystalPic.addClass("crystal-Pic");
-
-            //add an attribute for img src
-            crystalPic.attr("src", "http://www.crystalclearintentions.co.nz/wp-content/uploads/crystals.jpg");
-            //add a data attribute called crystal value
-
-            crystalPic.attr("data-crystalValue", crystalNumberOptions[i]);
-            $("#crystals").append(crystalPic);
-            console.log(crystalNumberOptions);
-        }
-    
+        
     //click event 
-    $(".crystal-pic").on("click", function() {
-        var crystalValue = ($(this).attr("data-crystalValue"));
-    
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
+    $("#crystal-1").on("click", function() {
         console.log(crystalValue);
-        alert("Points" + crystalValue);
-        // $("#total-score").text(counter);
+        crystalValue++;
+        $("#total-score").html(crystalValue);
+
+    
+        // crystalValue = parseInt(crystalValue);
+        // counter += crystalValue;
+        // console.log(crystalValue);
+        // alert("Points" + crystalValue);
+        // 
     })
     
-    // when I try to display total score it over rides the computer one. not sure why 
-    // also only one crystal has a num but it;s not working as expected 
-    //maybe make it an object that has a method where a random number is generated 
-    
+  
+  //game logic 
+  //if the player score equals computer score, then yay, you win. #nailed it
+  
+    //else if total score is greater than computer score, then a lose. but try again!
     // if (totalScore === computerNumber) {
     //     //console log and alert just for testing purposes 
     //     console.log("Yay, you win! Rock on. Click the refresh button to play again!");
