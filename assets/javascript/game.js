297007d80dc808f865a40b9a6ca936278ccdd24b
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-13 18:30:26 -0600

    added logic to dynamically update DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,58 +16,44 @@ $(document).ready(function() {
         console.log(computerNumber);
         
         //choices for random numbers for crystals --but this has to be for each one 
-        var crystalValue = Math.floor(Math.random() * (12-1));            
+        var crystalValue = Math.floor(Math.random() * (12-1));   
         
-      
+        //temp choices for random numbers
 
-//for each crystal, assign an attribute //
-
-$("#crystal-1").attr("data-crystalValue");
-
-// this attribute will contain the value of the random number 
-
-//each time I click a crystal, add crystal value to counter
-
+        crystalNumbers = [5,2,11,9];
         
-    
-        $("#random-number").text(computerNumber);
-        console.log(computerNumber);
-    
-        
-    //click event 
-    $("#crystal-1").on("click", function() {
-        console.log(crystalValue);
-        crystalValue++;
-        $("#total-score").html(crystalValue);
+        // dynamically update the DOM using a four loop 
+        for (var i = 0; i < crystalNumbers.length; i++ ) {
+                        //make an image 
 
-    
-        // crystalValue = parseInt(crystalValue);
-        // counter += crystalValue;
-        // console.log(crystalValue);
-        // alert("Points" + crystalValue);
-        // 
-    })
-    
-  
-  //game logic 
-  //if the player score equals computer score, then yay, you win. #nailed it
-  
-    //else if total score is greater than computer score, then a lose. but try again!
-    // if (totalScore === computerNumber) {
-    //     //console log and alert just for testing purposes 
-    //     console.log("Yay, you win! Rock on. Click the refresh button to play again!");
-    //     alert("Yay, you win! Rock on. Click the refresh button to play again!");
-    //     wins++;
-    // } else if 
-    //     (totalScore > computerNumber) {
-    //         console.log("Oh you, you lost. Click the refresh button to try again");
-    //         alert("Oh you, you lost. Click the refresh button to try again");
-    //         losses++;
+            var crystalImage = $("<img>");
+            //add a clas to it 
+         crystalImage.addClass = $("crystal-image");
+
+            //give each one attr src 
+
+            crystalImage.attr("src", "http://www.crystalclearintentions.co.nz/wp-content/uploads/crystals.jpg");
+
+            // give each one a dta attribute 
+
+            crystalImage.attr("data-crystalvalue",crystalNumbers[i]);
+
+            //add it to the page, to the crystals div
+            $("#crystals").append(crystalImage);
+        }
+
+        //click event 
+
+        $(".crystal-image").on("click", function() {
+            var crystalValue = ($(this).attr("data-crystalvalue"));
+            console.log(crystalValue);
+            $("#total-score").text(crystalValue);
             
-    //     }
-    
-    
-    
-    
-     });
+            // crystalValue = parseInt(crystalValue);
+            // counter += crystalValue;
 
+
+        
+        })
+        
+});
